ab88c9e0a505f29493be7e837dd140d2afd2d620
Jaka Kovačič <jakak2002@gmail.com>
2026-01-08 19:23:58 +0100

    Update motifs_tel.py

--- motifs_tel.py
+++ motifs_tel.py
@@ -6,30 +6,30 @@ model C_1_FFL_AND
   species X, Y, Z
 
   // X and Y dynamics
-  J0: -> X; kx
-  J1: -> Y; ky*(X/Kx)^n/(1 + (X/Kx)^n)
-  J2: X -> ; dx*X
+  //J0: -> X; kx
+  J1: -> Y; ky*(X/Kxy)^n/(1 + (X/Kxy)^n)
+  //J2: X -> ; dx*X
   J3: Y -> ; dy*Y
 
   // Z production
   // Uncomment one of these lines:
 
   // AND logic: Z requires both X and Y
-  J4: -> Z; kz*((X/Kx)^n/(1+(X/Kx)^n))*((Y/Ky)^n/(1+(Y/Ky)^n))
+  J4: -> Z; kz*((X/Kxz)^n/(1+(X/Kxz)^n))*((Y/Ky)^n/(1+(Y/Ky)^n))
 
   // OR logic: Z can be produced by either X or Y
-  //J4: -> Z; kz*((X/Kx)^n/(1+(X/Kx)^n) + (Y/Ky)^n/(1+(Y/Ky)^n))
+  //J4: -> Z; kz*((X/Kxz)^n/(1+(X/Kxz)^n) + (Y/Ky)^n/(1+(Y/Ky)^n))
 
   J5: Z -> ; dz*Z
 
   // Parameters
-  kx = 1.0; ky = 1.5; kz = 1.2
+  kx = 1.0; ky = 1.5; kz = 1.5
   dx = 0.5; dy = 0.5; dz = 0.5
-  Kx = 1.0; Ky = 1.0
+  Kxy = 1.0; Ky = 1.0; Kxz = 1.0
   n = 2
 
   // Initial conditions
-  X = 0.1; Y = 0.1; Z = 0.1
+  X = 0; Y = 1.5; Z = 1.05
 end
 """
 c_1_FFL_OR = """
@@ -37,30 +37,30 @@ model C_1_FFL_OR
   species X, Y, Z
 
   // X and Y dynamics
-  J0: -> X; kx
-  J1: -> Y; ky*(X/Kx)^n/(1 + (X/Kx)^n)
-  J2: X -> ; dx*X
+  //J0: -> X; kx
+  J1: -> Y; ky*(X/Kxy)^n/(1 + (X/Kxy)^n)
+  //J2: X -> ; dx*X
   J3: Y -> ; dy*Y
 
   // Z production
   // Uncomment one of these lines:
 
   // AND logic: Z requires both X and Y
-  //J4: -> Z; kz*((X/Kx)^n/(1+(X/Kx)^n))*((Y/Ky)^n/(1+(Y/Ky)^n))
+  //J4: -> Z; kz*((X/Kxz)^n/(1+(X/Kxz)^n))*((Y/Ky)^n/(1+(Y/Ky)^n))
 
   // OR logic: Z can be produced by either X or Y
-  J4: -> Z; kz*((X/Kx)^n/(1+(X/Kx)^n) + (Y/Ky)^n/(1+(Y/Ky)^n))
+  J4: -> Z; kz*((X/Kxz)^n/(1+(X/Kxz)^n) + (Y/Ky)^n/(1+(Y/Ky)^n))
 
   J5: Z -> ; dz*Z
 
   // Parameters
-  kx = 1.0; ky = 1.5; kz = 1.2
+  kx = 1.0; ky = 1.5; kz = 1.5
   dx = 0.5; dy = 0.5; dz = 0.5
-  Kx = 1.0; Ky = 1.0
+  Kxy = 1.0; Ky = 1.0; Kxz = 1.0
   n = 2
 
-  // Initial conditions
-  X = 0.1; Y = 0.1; Z = 0.1
+  // Initial conditionssitu
+  X = 1; Y = 1.5; Z = 3.5
 end
 """
 c_2_FFL_AND = """
@@ -207,22 +207,22 @@ model I_1_FFL_AND
   species X, Y, Z
 
   // X and Y dynamics
-  J0: -> X; kx
-  J1: -> Y; ky*(X/Kx)^n/(1 + (X/Kx)^n)
-  J2: X -> ; dx*X
+  //J0: -> X; kx
+  J1: -> Y; ky*(X/Kxy)^n/(1 + (X/Kxy)^n)
+  //J2: X -> ; dx*X
   J3: Y -> ; dy*Y
 
-  J4: -> Z; kz*((X/Kx)^n/(1+(X/Kx)^n) * 1/(1+(Y/Ky)^n))
+  J4: -> Z; kz*((X/Kxz)^n/(1+(X/Kxz)^n) * 1/(1+(Y/Ky)^n))
   J5: Z -> ; dz*Z
 
   // Parameters
-  kx = 1.0; ky = 1.5; kz = 1.2
+  kx = 1.0; ky = 1.5; kz = 5
   dx = 0.5; dy = 0.5; dz = 0.5
-  Kx = 1.0; Ky = 1.0
+  Kxy = 1.0; Ky = 1.0; Kxz = 1.0
   n = 2
 
   // Initial conditions
-  X = 0.1; Y = 0.1; Z = 0.1
+  X = 1; Y = 0.1; Z = 0.1
 end
 """
 i_2_FFL_AND = """
@@ -307,13 +307,13 @@ model PFL_1
   J3: Y -> ; dy*Y
 
   // Parameters
-  kx = 1.5; ky = 1.5 
-  dx = 0.5; dy = 0.5
-  Kx = 1.0; Ky = 1.0
+  kx = 2.0; ky = 1.0
+  dx = 0.4; dy = 0.4
+  Kx = 2.0; Ky = 1.0
   n = 2
 
   // Initial conditions
-  X = 1; Y = 0.1
+  X = 4; Y = 0.0
 end
 """
 PFL_2 = """
@@ -347,9 +347,9 @@ model NFL_1
   J3: Y -> ; dy*Y
 
   // Parameters
-  kx = 1.5; ky = 1.5 
-  dx = 0.5; dy = 0.5
-  Kx = 1.0; Ky = 1.0
+  kx = 0.5; ky = 0.5 
+  dx = 0.2; dy = 0.2
+  Kx = 0.5; Ky = 0.5
   n = 2
 
   // Initial conditions
@@ -381,7 +381,7 @@ end
 #AND
 PPFL_1 = """
 model PPFL
-  species X, Y, Z
+  species X, Z, Y
 
   // X and Y dynamics
   J0: -> X; kx*(Y/Ky)^n/(1 + (Y/Ky)^n)
@@ -392,13 +392,13 @@ model PPFL
   J5: Z -> ; dz*Z
 
   // Parameters
-  kx = 1.5; ky = 1.5; kz = 1.5
-  dx = 0.5; dy = 0.5; dz = 0.5
-  Kx = 1.0; Ky = 1.0; Kz = 1.0
+  kx = 2.0; ky = 1.0; kz = 2.0
+  dx = 0.4; dy = 0.4; dz = 0.4
+  Kx = 2.0; Ky = 1.0; Kz = 2.0
   n = 2
 
   // Initial conditions
-  X = 1; Y = 0.1; Z = 0.1
+  X = 4; Y = 0.0; Z = 0.0
 end
 """
 PNFL_1 = """
@@ -436,13 +436,13 @@ model NNFL
   J5: Z -> ; dz*Z
 
   // Parameters
-  kx = 1.5; ky = 1.5; kz = 1.5
-  dx = 0.5; dy = 0.5; dz = 0.5
-  Kx = 1.0; Ky = 1.0; Kz = 1.0
+  kx = 0.5; ky = 0.5; kz = 0.5
+  dx = 0.2; dy = 0.2; dz = 0.2
+  Kx = 0.5; Ky = 0.5; Kz = 0.5
   n = 2
 
   // Initial conditions
-  X = 1; Y = 0.1; Z = 0.1
+  X = 0.1; Y = 0.1; Z = 0.1
 end
 """
 
@@ -470,14 +470,16 @@ model represilator
 end
 """
 
+
+
 # Load model
-r = te.loada(rep)
+r = te.loada(NNFL_1)
 
 # Simulate from t=0 to t=50 with 200 points
-result = r.simulate(0, 50, 200)
+result = r.simulate(0, 15, 200)
 
 # Print first few results
 print(result[:10])
 
 # Plot dynamics
-r.plot(title="C-1 type FFL with AND", xtitle="Time", ytitle="[X]")
+r.plot(title="NN feedback loop", xtitle="Time", ytitle="[X]")
